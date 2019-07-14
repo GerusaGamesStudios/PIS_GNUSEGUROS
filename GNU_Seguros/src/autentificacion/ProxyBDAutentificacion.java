b2e83c9354d5024ed813ad08a39e0d60a1e34f6b
tealflipper <tealflipper@gmail.com>
2019-07-13 23:34:15 -0500

    inicio sesion sirve

--- GNU_Seguros/src/autentificacion/ProxyBDAutentificacion.java
+++ GNU_Seguros/src/autentificacion/ProxyBDAutentificacion.java
@@ -17,11 +17,12 @@ public class ProxyBDAutentificacion {
 	
 	//Las tablas seran arraylist por lo mientras
 	public ArrayList<Cuenta> cargarBD(){
-		return this.tablaCuentas;
+	    return this.tablaCuentas;
 	}
 	
-	public void GuardarBD(){
-		
+	public void GuardarBD(ArrayList<Cuenta> Tabla){
+            this.tablaCuentas=Tabla;
+                
 	}
 	
 	public Agente getCuentasAgente(){
@@ -29,9 +30,25 @@ public class ProxyBDAutentificacion {
 		
 	}
 	
-	public Cuenta getCuenta(){
+	public Cuenta getCuenta(String correo,String contraseña){
+            
+            for(int i=0;i<this.tablaCuentas.size();i++){
+                /*
+                 *compara para ver si el correo de alguna de las cuentas es igual
+                 *al correo dado
+                 */
+                if(this.tablaCuentas.get(i).getCorreo().compareTo(correo)==0){
+                    /*
+                    *Caso en el cual el correo de una cuenta corresponde al correo dado
+                    * Ahora compara si la contraseña de la cuenta es igual a la 
+                    * cotraseña dada
+                    */
+                    if(this.tablaCuentas.get(i).getContraseña().compareTo(contraseña)==0){
+                        return this.tablaCuentas.get(i);
+                    }
+                }else return null;
+            }
             return null;
-		
 	}
 	
 	public void addCuenta(Cuenta nueva){
