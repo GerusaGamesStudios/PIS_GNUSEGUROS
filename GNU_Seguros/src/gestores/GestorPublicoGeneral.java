d1903b407ea5119af63ed7a9a70b2839c6c415f8
tealflipper <tealflipper@gmail.com>
2019-07-13 23:34:30 -0500

    inicio sesion sirve

--- GNU_Seguros/src/gestores/GestorPublicoGeneral.java
+++ GNU_Seguros/src/gestores/GestorPublicoGeneral.java
@@ -8,6 +8,8 @@ package gestores;
 import autentificacion.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,19 +17,31 @@ import java.util.Scanner;
  */
 public class GestorPublicoGeneral {
 
-    private ProxyBDAutentificacion Tabla_a=new ProxyBDAutentificacion();
+    private ProxyBDAutentificacion Tabla_cuentas=new ProxyBDAutentificacion();
     private ArrayList<Cuenta> BD;
     private Scanner sc=new Scanner(System.in);
     
-    public Cuenta iniciarSesion(){
-        String correo=new String();
+    public Cuenta iniciarSesion(String correo, String contraseña){
         boolean correoVerificado=false;
-        System.out.println("Ingrese el correo electronico");
-        correo=sc.nextLine();
         correoVerificado=verificarCorreo(correo);
-        if(!correoVerificado) return null;
-        return null;
+        if(!correoVerificado){
+            System.out.println("Correo incorrecto");
+            
+            //un delay de un segundo
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(GestorPublicoGeneral.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            //regresa referencia a nulo si no esta la cuenta o contraseña no es la misma
+            return null;
+        }else{
+            BD=Tabla_cuentas.cargarBD();
+            return Tabla_cuentas.getCuenta(correo,contraseña);
+        }
     }
+    
+    
     private boolean verificarCorreo(String correo){
         for(int i=0; i<correo.length();i++){
             if(correo.charAt(i)=='@') return true;
@@ -40,19 +54,35 @@ public class GestorPublicoGeneral {
     public static void main(String [ ] args){
         GestorPublicoGeneral gestorGen=new GestorPublicoGeneral();
         Scanner sc=new Scanner(System.in);
+        Cuenta sesion;
         int opcion;
-        System.out.println("------GNU Seguros------");
-        System.out.println("(1): Inicio sesion");
-        System.out.println("(2): Ver lista publica de agentes");
-        System.out.println("(3): Visualizar seguros");
-        System.out.println("(4): Cotizar seguro");
-        System.out.println("(5): Consultar informacion");
-        System.out.println("(6): Salir");
-        System.out.print("\nOpcion: ");
-        opcion=sc.nextInt();
-        switch(opcion){
-            case(1):
-                gestorGen.iniciarSesion();
+        boolean continuar=true;
+        String correo;
+        String contraseña;
+        while(continuar){
+            System.out.println("------GNU Seguros------");
+            System.out.println("(1): Inicio sesion");
+            System.out.println("(2): Ver lista publica de agentes");
+            System.out.println("(3): Visualizar seguros");
+            System.out.println("(4): Cotizar seguro");
+            System.out.println("(5): Consultar informacion");
+            System.out.println("(6): Salir");
+            System.out.print("\nOpcion: ");
+            opcion=sc.nextInt();
+            switch(opcion){
+                case(1):
+                    System.out.print("Ingresar correo: ");
+                    correo=sc.nextLine();
+                    System.out.print("Ingresar contraseña: ");
+                    contraseña=sc.nextLine();
+                    sesion=gestorGen.iniciarSesion(correo,contraseña);
+                    break;
+                case(6):
+                  continuar=false;
+                  break;
+              default: break;
+            }  
         }
+        
     }
 }
