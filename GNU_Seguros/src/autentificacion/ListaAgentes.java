bd4c0854943b7be5ec15f8f82962e40820476cea
Gerardo Torres <gerardotorres@MacBook-Air-de-Gerardo.local>
2019-07-16 21:57:42 -0500

    Agregado metodos addagante y delete agente

--- GNU_Seguros/src/autentificacion/ListaAgentes.java
+++ GNU_Seguros/src/autentificacion/ListaAgentes.java
@@ -10,5 +10,17 @@ package autentificacion;
  * @author tealflipper
  */
 public class ListaAgentes implements java.io.Serializable {
-    
+  private ArrayList<Agente> ListaAgentes = new ArrayList<Agente>();
+  public void addAgente(Agente a){
+    this.listaAgentes.add(a);
+  }
+  public void deleteAgente(String str){
+    for(int i = 0; i < this.ListaAgentes.lenght;i++){
+      if(this.ListaAgentes[i].getNumeroAgente() == str){
+        this.ListaAgentes.remove(i);
+        return;
+      }
+    }
+  }
+
 }
